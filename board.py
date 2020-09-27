38b52a1e869b57c5e3d6ceb2f2f8e95bbba1cd95
Rasmus <39956903+Visceral89@users.noreply.github.com>
2020-09-27 19:23:49 +0200

    First update on board.py

--- board.py
+++ board.py
@@ -1,1 +1,11 @@
 #here is where im going to generate the code to make the board and the window
+import pygame
+
+# Init
+pygame.init()
+
+# Variables
+dis_width = 600
+dis_height = 600
+dis = pygame.display.set_mode((dis_width, dis_width))
+pygame.display.set_caption("Snake av Rasmus")
